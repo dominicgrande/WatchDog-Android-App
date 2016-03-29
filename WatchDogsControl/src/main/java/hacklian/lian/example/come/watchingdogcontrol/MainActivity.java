2c503f4979712102814ec2ca6e2b0c8bfafc0a9a
dominicgrande <dominic.c.grande@gmail.com>
2016-03-28 21:12:29 -0500

    What was finished by end of Hackathon

--- WatchDogsControl/src/main/java/hacklian/lian/example/come/watchingdogcontrol/MainActivity.java
+++ WatchDogsControl/src/main/java/hacklian/lian/example/come/watchingdogcontrol/MainActivity.java
@@ -20,9 +20,21 @@ import android.widget.VideoView;
 
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.UUID;
@@ -32,17 +44,25 @@ public class MainActivity extends AppCompatActivity {
     private final static UUID PEBBLE_APP_UUID = UUID.fromString("044a5b16-3fd7-47db-b6c5-dbe4e90f2d44");
     private TextView outputTest;
     private Handler mHandler = new Handler();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+        VideoView vidView = (VideoView) findViewById(R.id.myVideo);
         String vidAddress = "https://mediasvclkmn5vd19lrm9.blob.core.windows.net/asset-cb15435d-1500-80c5-6d49-f1e5d84ed36b/20160109_144411.mp4?sv=2012-02-12&sr=c&si=9483c9d6-40ca-4b18-aead-d87dcccad661&sig=DoBmVcT4CB209PdOps8ZDgGC9tyMiZQmG%2Fk8Zh4tDqk%3D&st=2016-02-21T03%3A53%3A57Z&se=2116-01-28T03%3A53%3A57Z";
         Uri vidUri = Uri.parse(vidAddress);
         vidView.setVideoURI(vidUri);
         vidView.start();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void onResume() {
@@ -87,46 +107,82 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-    private class CallAPI extends AsyncTask<String, String, String> {
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "iot.eclipse.org:1883",
+                        clientId);
 
-        @Override
-        protected String doInBackground(String... params) {
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                String TAG;
 
-            String urlString=params[0]; // URL to call
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                }
 
-            String resultToDisplay = "";
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
 
-            InputStream in = null;
-
-            // HTTP Get
-            try {
-
-                URL url = new URL(urlString);
-
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                in = new BufferedInputStream(urlConnection.getInputStream());
-
-            } catch (Exception e ) {
-
-                System.out.println(e.getMessage());
-
-                return e.getMessage();
-
-            }
-
-            return resultToDisplay;
-
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
 
-        protected void onPostExecute(String result) {
-
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
+        try {
+            IMqttToken token = client.connect(options);
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
 
-    } // end CallAPI
+        String topic = "Direction";
+        String payload = "Right";
+        byte[] encodedPayload = new byte[0];
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://hacklian.lian.example.come.watchingdogcontrol/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -150,13 +206,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void turnRight(View view)
-    {
-      return;
+    public void turnRight(View view) {
+        return;
     }
 
-    public void turnLeft(View view)
-    {
+    public void turnLeft(View view) {
         return;
     }
 }
